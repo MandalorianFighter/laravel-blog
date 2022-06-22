fdd39b63e8f8a0380a5a5e5a3f2b87d8f224e3fa
MandalorianFighter <yanush.polishchuk@gmail.com>
2022-06-22 17:52:56 +0300

    modify app/Models/Article.php

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     use HasFactory;
+
+    public function scopeIsPublished($query)
+{
+    return $query->where('published', true);
+}
 }
