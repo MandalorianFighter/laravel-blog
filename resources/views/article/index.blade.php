7c926fdcf46ba39f630cdc13437fe73574256994
MandalorianFighter <yanush.polishchuk@gmail.com>
2022-06-24 12:38:40 +0300

    modify article/index.blade.php

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -5,8 +5,19 @@
 @section('header', 'Articles list')
 
 @section('content')
+{{Form::open(['url' => route('articles'), 'method' => 'GET'])}}
+    {{Form::text('q', $q)}}
+    {{Form::submit('Find!')}}
+{{Form::close()}}
     @foreach ($articles as $article)
         <h2><a href="/articles/{{$article->id}}">{{$article->name}}</a></h2>
         <div>{{Str::limit($article->body, 200)}}</div>
     @endforeach
+
+{{ $articles->links() }}
+<style>
+    .w-5{
+        display: none;
+    }
+</style>
 @endsection
