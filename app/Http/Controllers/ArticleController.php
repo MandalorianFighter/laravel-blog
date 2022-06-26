a78069e55804c59d3a7836a5feaa448c3002744f
MandalorianFighter <yanush.polishchuk@gmail.com>
2022-06-26 10:53:03 +0300

    modify ArticleController.php

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -12,14 +12,38 @@ class ArticleController extends Controller
     public function index(Request $request)
     {
         $q = $request->input('q');
+        
+        $success = $request->session()->get('success');
+        
         $articles = Article::where('name', 'like', "%{$q}%")->paginate();
 
-        return view('article.index', compact('articles', 'q'));
+        return view('article.index', compact('articles', 'q', 'success'));
     }
 
     public function show($id)
     {
         $article = Article::findOrFail($id);
         return view('article.show', compact('article'));
+    }
+
+    public function create()
+    {
+        $article = new Article();
+        return view('article.create', compact('article'));
+    }
+
+    public function store(Request $request)
+    {
+        $data = $this->validate($request, [
+            'name' => 'required|unique:articles',
+            'body' => 'required|min:1000',
+        ]);
+
+        $article = new Article();
+        $article->fill($data);
+        $article->save();
+
+        return redirect()
+            ->route('articles.index')->with('success','Article created successfully');
     }
 }
