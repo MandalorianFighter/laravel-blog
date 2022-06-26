9328327838b227178e154e238e278360e4ac8a30
MandalorianFighter <yanush.polishchuk@gmail.com>
2022-06-26 17:40:58 +0300

    add resources/views/article/form.blade.php

---
+++ resources/views/article/form.blade.php
@@ -0,0 +1,14 @@
+@if ($errors->any())
+    <div>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+{{ Form::label('name', 'Name') }}
+{{ Form::text('name') }}<br>
+{{ Form::label('body', 'Content') }}
+{{ Form::textarea('body') }}<br>
